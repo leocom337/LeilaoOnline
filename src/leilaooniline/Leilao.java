f33715bf04622e4035d9f5ad9feb31deba9d383b
Letícia <Letcia@DESKTOP-SDJOSJV>
2020-04-08 19:52:11 -0300

    Feat cria Leilão

---
+++ src/leilaooniline/Leilao.java
@@ -0,0 +1,34 @@
+package leilaooniline;
+
+import java.util.Calendar;
+
+
+public class Leilao {
+  private  int idLeilao;
+  private  Calendar dataLeilao;
+  private Double ultimoLance;
+
+    public int getIdLeilao() {
+        return idLeilao;
+    }
+
+    public void setIdLeilao(int idLeilao) {
+        this.idLeilao = idLeilao;
+    }
+
+    public Double getUltimoLance() {
+        return ultimoLance;
+    }
+
+    public void setUltimoLance(Double ultimoLance) {
+        this.ultimoLance = ultimoLance;
+    }
+    
+    public void pesquisar(){
+        
+    }
+    public void finalizarCompra(){
+                                  
+    }
+    
+}

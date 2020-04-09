a6bf1b6f3a500ad343b68a00d3f019f2302e899c
55319 <55319@DESKTOP-1M6CSE8>
2020-04-08 21:27:28 -0300

    feature: criar classe pagamento;

---
+++ src/leilaooniline/Pagamento.java
@@ -0,0 +1,41 @@
+,lpackage diagnostica2;
+
+
+public class pagamento {
+    
+       private int qtdeParcelas;
+    
+    private  Integer AVISTA = 1;
+    private  Integer APRAZO = 2;
+    private  calendar dataPagamento;    
+    private  Double valorPagamento;
+            
+    public Pagamento(int qtdeParcelas, String data, String descricao, double valor) {
+        super(data, descricao, valor);
+        this.qtdeParcelas = qtdeParcelas;
+    }
+
+   
+    public Pagamento(int qtdeParcelas) {
+        this.qtdeParcelas = qtdeParcelas;
+    }
+
+    public int getQtdeParcelas() {
+        return qtdeParcelas;
+    }
+
+  
+    public void setQtdeParcelas(int qtdeParcelas) {
+        this.qtdeParcelas = qtdeParcelas;
+    }
+
+  
+    public String toString() {
+        return "Compra{" + data + ", " + descricao + ", " + valor + "(D), " + qtdeParcelas +"  }";
+    }
+    
+    
+    
+    
+}        
+
